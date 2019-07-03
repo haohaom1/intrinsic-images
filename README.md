8666ba8b879793d76bac342176aecba23ff4d997
Allen Ma <riallenma@gmail.com>
2019-07-03 16:59:32 -0400

    update file directory and documented in readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # intrinsic-images
+
+
+# Explanation of data directory
+
+* data
+    * mmap; material maps
+        * **mmap_npy**; 512x512 material maps in numpy array form
+        * **mmap_real_cr2**; Canon raw, uncropped images of material maps
+        * **mmap_real_npy**; uncropped raw material images converted into numpy arrays from 16-bit (linear) tiff
+        * **mmap_real_tiff**; 16-bit linear tiffs of the Canon raw images
+        * **mmap_synthetic**; 512x512 ppms generated using Bruce's C code
+    * imap; illumination map
+        * **imap_npy**; 512x512 illumination maps
+        * **imap_npy_ambient**; 512x512 ambient illumination maps
+        * **imap_npy_direct**; 512x512 direct illumination maps
+        * **imap_real_cr2**; Canon raw, uncropped images of illumination maps
+        * **imap_real_npy**; uncropped raw illumination images converted into numpy arrays from 16-bit (linear) tiff
+        * **imap_real_tiff**; 16-bit linear tiffs of the Canon raw images
--- janknet_separation.py
+++ janknet_separation.py
@@ -29,56 +29,90 @@ from keras.losses import mse
 
 # true image is the illumination map that was used to construct the input image
 # pred image is the generated illumination map * 0.5 
-def custom_loss(true_img, pred_img):
-   return K.mean(K.square(true_img * 0.5 - pred_img))
+
 
 input_img = Input(shape=(128, 128, 3))  # adapt this if using `channels_first` image data format
 
-# encoder layer
-x = Conv2D(8, (3, 3), activation='selu', padding='same')(input_img)
-x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(16, (3, 3), activation='selu', padding='same')(x)
-x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(32, (3, 3), activation='selu', padding='same')(x)
-x = Dropout(0.2,name='drop1')(x)
-x = Conv2D(32, (3, 3), activation='selu', padding='same')(x)
-x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(64, (3, 3), activation='selu', padding='same')(x)
-x = Conv2D(32, (1, 1), activation='sigmoid', padding='same')(x)
-encoded = MaxPooling2D((2, 2), padding='same')(x)
-# ensure everything is between 0 and 1
+class JankNet():
+    def __init__(self):
+        # encoder layer
+        x = Conv2D(8, (3, 3), activation='selu', padding='same')(input_img)
+        x = MaxPooling2D((2, 2), padding='same')(x)
+        x = Conv2D(16, (3, 3), activation='selu', padding='same')(x)
+        x = MaxPooling2D((2, 2), padding='same')(x)
+        x = Conv2D(32, (3, 3), activation='selu', padding='same')(x)
+        x = Dropout(0.2,name='drop1')(x)
+        x = Conv2D(32, (3, 3), activation='selu', padding='same')(x)
+        x = MaxPooling2D((2, 2), padding='same')(x)
+        x = Conv2D(64, (3, 3), activation='selu', padding='same')(x)
+        x = Conv2D(32, (1, 1), activation='sigmoid', padding='same')(x)
+        encoded = MaxPooling2D((2, 2), padding='same')(x)
+        # ensure everything is between 0 and 1
 
-# at this point the representation is (16, 16, 32) i.e. 2048-dimensional
+        # at this point the representation is (16, 16, 32) i.e. 2048-dimensional
 
-x = Conv2D(32, (3, 3), activation='selu', padding='same')(encoded)
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(16, (3, 3), activation='selu', padding='same')(x)
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(8, (3, 3), activation='selu', padding='same')(x)
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(1, (3, 3), activation='selu', padding='same')(x)
-x = UpSampling2D((2, 2))(x)
-decoded_imap = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)
-
-
-separator = Model(input_img, decoded_imap)
-separator.compile(optimizer='adam', loss=custom_loss, metrics=['mse'])
+        x = Conv2D(32, (3, 3), activation='selu', padding='same')(encoded)
+        x = UpSampling2D((2, 2))(x)
+        x = Conv2D(16, (3, 3), activation='selu', padding='same')(x)
+        x = UpSampling2D((2, 2))(x)
+        x = Conv2D(8, (3, 3), activation='selu', padding='same')(x)
+        x = UpSampling2D((2, 2))(x)
+        x = Conv2D(1, (3, 3), activation='selu', padding='same')(x)
+        x = UpSampling2D((2, 2))(x)
+        decoded_imap = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)
 
 
+        self.model = Model(input_img, decoded_imap)
+        self.model.compile(optimizer='adam', loss=self.imap_only_loss, metrics=['mse'])
+        
+        
+    def imap_only_loss(self, true_img, pred_img):
+       return K.mean(K.square(true_img * 0.5 - pred_img))
 
-separator.summary()
 
-# checkpoint
-filepath="./weights-improvement-{epoch:02d}-{loss:.2f}.hdf5"
-# save the minimum loss
-checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=False)
-callbacks_list = [checkpoint]
-# Fit the model
+    def __str__(self):
+        return self.model.summary()
+    
+        def train(self, len_data, batch_size, num_epochs, gen, callbacks_list=[]):
+        '''
+            call this to train the network
+            gen - a generator function to pass into model.fit_generator()
+        '''
+        self.model.fit_generator(gen, steps_per_epoch= len_data / batch_size, epochs=num_epochs, verbose=2, callbacks=callbacks_list)
+        
+        
+def main():
 
-separator.fit(x_train, [vert_train, horiz_train],
-                epochs=30,
-                batch_size=84,
-                shuffle=True,
-                validation_split=0.1,
-                callbacks=callbacks_list)
+    # NOTE: hardcoding is as follows
+    # imap_npy has 4800 images
+    # mmap_npy has 1200 images
+    # so by design, we multiply mmap * 4
+    # batch size 64 bc 4800 / 64 = 75 which is cleanly divisible
+    # file paths are hardcoded for the linux dwarves
+    # - Allen 2 July
+
+    unet = JankNet()
+
+    # hardcoded path names
+    # in data_gen you apparently have to specify final
+    path_imap = "/media/yma21/gilmore/intrinsic-images/data/imap_npy/final"
+    path_mmap = "/media/yma21/gilmore/intrinsic-images/data/matmap_npy/"
+
+    BATCH_SIZE = 64
+    LEN_DATA = 4800
+    EPOCHS = 50
+
+    # checkpoint
+    filepath="./weights-janknet-{epoch:02d}-{loss:.2f}.hdf5"
+    # save the minimum loss
+    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=2, save_best_only=False)
+    callbacks_list = [checkpoint]
+    # Fit the model
+    unet.train(LEN_DATA, BATCH_SIZE, EPOCHS, data_gen.generator(path_imap, path_mmap), callbacks_list)
+
+
+if __name__ == "__main__":
+    main()
+
+
 
