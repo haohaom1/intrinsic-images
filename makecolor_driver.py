7b603c6464e9b3c23a237d28c1ca95cbf2cb0815
Mike Fu <xuanhaofu@gmail.com>
2019-07-09 16:01:30 -0400

    updated drivers

--- makecolor_driver.py
+++ makecolor_driver.py
@@ -1,6 +1,6 @@
 import os
 
-os.system("python3 makecolor.py stripe 30000 train 512 512")
-os.system("python3 makecolor.py perlin 30000 train 512 512")
-os.system("python3 makecolor.py fractal 30000 train 512 512")
-os.system("python3 makecolor.py random 30000 train 512 512")
+os.system("python3 makecolor.py stripe 7500 train 512 512")
+os.system("python3 makecolor.py perlin 7500 train 512 512")
+os.system("python3 makecolor.py fractal 7500 train 512 512")
+os.system("python3 makecolor.py random 7500 train 512 512")
--- makecolor_driver_test.py
+++ makecolor_driver_test.py
@@ -1,6 +1,6 @@
 import os
 
-os.system("python3 makecolor.py stripe 10000 test 512 512")
-os.system("python3 makecolor.py perlin 10000 test 512 512")
-os.system("python3 makecolor.py fractal 10000 test 512 512")
-os.system("python3 makecolor.py random 10000 test 512 512")
+os.system("python3 makecolor.py stripe 2500 test 512 512")
+os.system("python3 makecolor.py perlin 2500 test 512 512")
+os.system("python3 makecolor.py fractal 2500 test 512 512")
+os.system("python3 makecolor.py random 2500 test 512 512")
