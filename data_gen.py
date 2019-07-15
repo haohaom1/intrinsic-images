6b51447d39a9d7743527b3fbe73256469ba69cf4
Allen Ma <riallenma@gmail.com>
2019-07-15 14:27:28 -0400

    update generator

--- data_gen.py
+++ data_gen.py
@@ -16,7 +16,7 @@ import cv2
 # NOTE: zip takes care of the nondivisible issue, anything that is extra 
 # from either list is truncated off 
 
-def generator(path_imap, path_mmap, log=False, num_imaps_per_mmap=4, resolution=128, batch_size = 64):
+def generator(path_imap, path_mmap, log=False, num_imaps_per_mmap=4, resolution=128):
 
     '''
     Takes two paths, and creates a generator
@@ -37,47 +37,48 @@ def generator(path_imap, path_mmap, log=False, num_imaps_per_mmap=4, resolution=
         # without replacement
         for file_mmap, file_imap in zip(all_mmap_files, imap_files):
 
-            batch_imap = []
-            batch_res = []
-            # this will fetch a batch
-            for _ in range(batch_size):
 
-                # ASSUMES THAT THE PATH STRUCTURE IS NAMING CONVENTION: NAMING CONVENTION: ./data/[(ambient_imap, direct_imap, imap)]/[train, test]/[gen_type]%d.npy
-                amb_imap = np.load(os.path.join(path_imap.replace('imap_npy', 'imap_npy_ambient'), file_imap), allow_pickle=True)
-                dir_imap = np.load(os.path.join(path_imap.replace('imap_npy', 'imap_npy_direct'), file_imap), allow_pickle=True)
+            # ASSUMES THAT THE PATH STRUCTURE IS NAMING CONVENTION: NAMING CONVENTION: ./data/imap/[imap_npy, imap_npy_ambient, imap_npy_direct]/[train, test]/[gen_type]%d.npy
+            # amb_imap = np.load(os.path.join(path_imap.replace('imap_npy', 'imap_npy_ambient'), file_imap), allow_pickle=True)
+            # dir_imap = np.load(os.path.join(path_imap.replace('imap_npy', 'imap_npy_direct'), file_imap), allow_pickle=True)
 
-                mmap = np.load(os.path.join(path_mmap, file_mmap), allow_pickle=True)
-                imap = np.load(os.path.join(path_imap, file_imap), allow_pickle=True)
-                    
-                assert(mmap.shape == imap.shape)
-                res = np.multiply(mmap, imap)  # element wise multiplication
+            mmap = np.load(os.path.join(path_mmap, file_mmap), allow_pickle=True)
+            imap = np.load(os.path.join(path_imap, file_imap), allow_pickle=True)
+                
+            assert(mmap.shape == imap.shape)
+            res = np.multiply(mmap, imap)  # element wise multiplication
 
-                # cutoff between 0 and 1
-                # because image values can only be between 0 and 1
-                # but real-world data can be larger
-                res = np.clip(res, 0., 1.)
+            # cutoff between 0 and 1
+            # because image values can only be between 0 and 1
+            # but real-world data can be larger
+            res = np.clip(res, 0., 1.)
 
-                # if using logspace, convert to 16 bit ints, add offset, then take log
-                if log:
-                    offset = 5
-                    res = res * 65535 + offset
+            # if using logspace, convert to 16 bit ints, add offset, then take log
+            if log:
+                offset = 5
+                res = res * 65535 + offset
 
-                    res = np.log(res)
+                res = np.log(res)
 
-                # if using linear space, scale all values between [-0.5, 0.5]
-                else:
-                    res -= 0.5
+            # # if using linear space, scale all values between [-0.5, 0.5]
+            # else:
+            #     res -= 0.5
 
-                assert(res.shape == imap.shape)
+            assert(res.shape == imap.shape)
 
-                # rescale
-                imap_cropped = cv2.resize(imap, (resolution, resolution), interpolation=cv2.INTER_AREA)
-                res_cropped = cv2.resize(res, (resolution, resolution), interpolation=cv2.INTER_AREA)
-
-                batch_res.append(res_cropped)
-                batch_imap.append(imap_cropped)
+            # rescale
+            imap_cropped = cv2.resize(imap, (resolution, resolution), interpolation=cv2.INTER_AREA)
+            res_cropped = cv2.resize(res, (resolution, resolution), interpolation=cv2.INTER_AREA)
 
             # yield amb_imap, dir_imap, imap, mmap, res
+            yield res, imap, mmap
 
-            # only compare with imap
-            yield np.array(batch_res), np.array(batch_imap)  
+def generator_batch(gen, batch_size=64):
+
+    # gets list of res, imaps, mmaps with size = batch_size
+    results = [next(gen) for _ in range(batch_size)]
+
+    # splits up the lists for unpacking and turns them into np arrays
+    final = tuple([np.array(l) for l in zip(*results)])
+
+    return final
