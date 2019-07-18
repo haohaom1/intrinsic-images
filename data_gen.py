0cf252a19256c354392088de2359726c98e86e7b
Allen Ma <riallenma@gmail.com>
2019-07-18 12:23:25 -0400

    Revert "datagen can now return amb dir and mmaps"
    This reverts commit 3901874864c33287c7e607356d59c5516dde95b0.

--- data_gen.py
+++ data_gen.py
@@ -10,26 +10,16 @@ import cv2
 # from either list is truncated off 
 
 
-def generator(imap_files, mmap_files, path_mmap, path_imap, valid_len_data, 
-              inputs_to_network=None, ground_truth=None,
-              log=False, resolution=128, batch_size=64):
+def generator(imap_files, mmap_files, path_mmap, path_imap, valid_len_data, log=False, resolution=128, batch_size=64):
 
     '''
     Takes two paths, and creates a generator to yield batches of data
     Can be used for training and validation data
     # Arguments
-        imap_files = a list of filenames (strings) of npy files each containing an illumination map (pass in the actual imap, not ambient or direct)
+        imap_files = a list of filenames (strings) of npy files each containing an illumination map
         mmap_files = a list of filenames (strings) of npy files, each containing a material map
-            NOTE that mmap_files has the number of imaps_per_mmap PRE-MULTIPLIED, so there will be NUM_IMAPS_PER_MMAP copies of each mmap npy
+            Note that mmap_files has the number of imaps_per_mmap PRE-MULTIPLIED, so there will be NUM_IMAPS_PER_MMAP copies of each mmap npy
         valid_len_data = the valid length of the data, should be fully divisible by batch size
-
-        inputs_to_network = a list of which types of images to yield in the generator that are fed into the network. 
-                Select from ['ambient', 'direct', 'imap', 'mmap', 'result'].
-                Default to returning the result
-        ground_truth = a list of which types of images to yield in the generator that are used as the ground truth
-                Select from ['ambient', 'direct', 'imap', 'mmap', 'result'].
-                Default to returning the imap
-
         log = default False. If true, then it takes the log of all the images, and adds a small shift to ensure the values are > 1
         resolution = default 128, square size of the image
         batch_size = default 64. batch size to use for training
@@ -68,19 +58,10 @@ def generator(imap_files, mmap_files, path_mmap, path_imap, valid_len_data,
             batch_res = []
             batch_imap = []
             batch_mmap = []
-            batch_amb = []
-            batch_dir = []
-
-            path_amb = path_imap.replace('imap_npy', 'imap_npy_ambient')
-            path_dir = path_imap.replace('imap_npy', 'imap_npy_direct')
-
             for file_mmap, file_imap in batch_files:
 
                 mmap = np.load(os.path.join(path_mmap, file_mmap), allow_pickle=True)
                 imap = np.load(os.path.join(path_imap, file_imap), allow_pickle=True)
-                ambient = np.load(os.path.join(path_amb, file_imap), allow_pickle=True)
-                direct = np.load(os.path.join(path_dir, file_imap), allow_pickle=True)
-                
 
                 assert(mmap.shape == imap.shape)
                 res = np.multiply(mmap, imap)  # element wise multiplication
@@ -106,51 +87,14 @@ def generator(imap_files, mmap_files, path_mmap, path_imap, valid_len_data,
                 # resize by rescaling
                 res_cropped = cv2.resize(res, (resolution, resolution), interpolation=cv2.INTER_AREA)
                 imap_cropped = cv2.resize(imap, (resolution, resolution), interpolation=cv2.INTER_AREA)
-                mmap_cropped = cv2.resize(mmap, (resolution, resolution), interpolation=cv2.INTER_AREA)
-                ambient_cropped = cv2.resize(ambient, (resolution, resolution), interpolation=cv2.INTER_AREA)
-                direct_cropped = cv2.resize(direct, (resolution, resolution), interpolation=cv2.INTER_AREA)
+                # mmap_cropped = cv2.resize(mmap, (resolution, resolution), interpolation=cv2.INTER_AREA)
 
                 batch_res.append(res_cropped)
                 batch_imap.append(imap_cropped)
-                batch_mmap.append(mmap_cropped)
-                batch_amb.append(ambient_cropped)
-                batch_dir.append(direct_cropped)
-
-            # convert to npy arrays
-            batch_res = np.array(batch_res)
-            batch_imap = np.array(batch_imap)
-            batch_mmap = np.array(batch_mmap)
-            batch_amb = np.array(batch_amb)
-            batch_dir = np.array(batch_dir)
-
-            inputs = []
-            gtruth = []
-
-            param_dict = dict(zip(['ambient', 'direct', 'imap', 'mmap', 'result'], 
-                                  [batch_amb, batch_dir, batch_imap, batch_mmap, batch_res]))
-
-            # loop through inputs_to_network to yield the correct inputs to network
-            if not inputs_to_network:       # default value, inputs_to_network is None
-                inputs = batch_res
-            elif len(inputs_to_network) == 1:  # if there is only one value needed, dont need list
-                inputs = param_dict[inputs_to_network[0]]
-            else:
-                for val in inputs_to_network:
-                    inputs.append(param_dict[val])
+                # batch_mmap.append(mmap_cropped)
             
-            # repeat for ground truth
-            if not ground_truth:       # default value, inputs_to_network is None
-                gtruth = batch_imap
-            elif len(ground_truth) == 1:  # if there is only one value needed, dont need list
-                gtruth = param_dict[ground_truth[0]]
-            else:
-                for val in ground_truth:
-                    gtruth.append(param_dict[val])
-            
-            yield inputs, gtruth
-
-            # # in the future, need to return 5, need to take account of this inside model.train()
-            # yield np.array(batch_res), np.array(batch_imap) #, np.array(batch_mmap)
+            # in the future, need to return 5, need to take account of this inside model.train()
+            yield np.array(batch_res), np.array(batch_imap) #, np.array(batch_mmap)
 
 
             
