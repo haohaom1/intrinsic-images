1d90bf81421c0eff0c75f44cd33cd9fb742c63e1
Allen Ma <riallenma@gmail.com>
2019-07-18 16:06:55 -0400

    update driver

--- model_train_driver.py
+++ model_train_driver.py
@@ -9,7 +9,7 @@ import sys
 
 def main(argv):
     if len(argv) < 6:
-        print("[user] [device name] [batch size] [epochs] [num_imaps_per_mmap] [model name]")
+        print("[user] [device name] [batch size] [epochs] [num_imaps_per_mmap] [model name] [no_validation (true = no_validation)]")
 
     user = argv[1]
     device = argv[2]
@@ -17,9 +17,17 @@ def main(argv):
     epoch = argv[4]
     num = argv[5]
     model = argv[6]
+    no_validation = argv[7]
 
-    os.system(f"python3 model_train.py /media/{user}/{device}/intrinsic-images/data/imap/imap_npy/train/ \
-        /media/{user}/{device}/intrinsic-images/data/mmap/mmap_npy/train/ {batch} {epoch} {num} {model}")
+
+    if no_validation == "true":
+        callstring = f"python3 model_train.py --no_validation /media/{user}/{device}/intrinsic-images/data/imap/imap_npy/train/ \
+        /media/{user}/{device}/intrinsic-images/data/mmap/mmap_npy/train/ {batch} {epoch} {num} {model}"
+    else:
+        callstring = f"python3 model_train.py /media/{user}/{device}/intrinsic-images/data/imap/imap_npy/train/ \
+        /media/{user}/{device}/intrinsic-images/data/mmap/mmap_npy/train/ {batch} {epoch} {num} {model}"
+        
+    os.system(callstring)
 
 if __name__ == "__main__":
     main(sys.argv)
