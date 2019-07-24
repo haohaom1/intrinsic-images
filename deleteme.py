89c759ef2b4c975791c741918a56475a59b18ad4
Mike Fu <xuanhaofu@gmail.com>
2019-07-24 12:33:16 -0400

    test file

--- deleteme.py
+++ deleteme.py
@@ -9,5 +9,7 @@ files = random.shuffle(os.listdir(directory))[:100]
 imgs = [np.load(os.path.join(directory, x), allow_pickle=True) for x in files if x.endswith('.npy')]
 
 fig, axes = plt.subplots(10, 10, figsize=(10,10))
-for im, ax in zip(imgs, axes.flatten()):
+for i, (im, ax, f) in enumerate(zip(imgs, axes.flatten(), files)):
+    print(f, i)
+    ax.set_title(f)
     ax.imshow(im)
