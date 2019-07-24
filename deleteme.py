92b1b603ed82f16fef24571e7e7f24973d6f74f9
Mike Fu <xuanhaofu@gmail.com>
2019-07-24 13:19:24 -0400

    test file

--- deleteme.py
+++ deleteme.py
@@ -2,19 +2,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import random
+import sys
 
-directory = '/media/yma21/milton/intrinsic-images/data/imap/imap_npy/train'
-files = os.listdir(directory)
-random.shuffle(files)
-files = files[:100]
+def main(argv):
 
-imgs = [np.load(os.path.join(directory, x), allow_pickle=True) for x in files if x.endswith('.npy')]
+    if len(argv) < 2:
+        print("[name of directoy]")
+        return
 
-fig, axes = plt.subplots(10, 10, figsize=(10,10))
-for i, (im, ax, f) in enumerate(zip(imgs, axes.flatten(), files)):
-    print(f, i)
-    ax.set_title(f)
-    ax.imshow(im)
+    directory = argv[1]
 
-# plt.tight_layout()
-plt.show()
+    files = os.listdir(directory)
+    random.shuffle(files)
+    files = files[:100]
+
+    imgs = [np.load(os.path.join(directory, x), allow_pickle=True) for x in files if x.endswith('.npy')]
+
+    fig, axes = plt.subplots(10, 10, figsize=(10,10))
+    for i, (im, ax, f) in enumerate(zip(imgs, axes.flatten(), files)):
+        print(f, i)
+        ax.set_title(f)
+        ax.imshow(im)
+
+    # plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    main(sys.argv)
