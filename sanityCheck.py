e3b5a71e85d45b28374c4f0d09ba4e34473523a4
Mike Fu <xuanhaofu@gmail.com>
2019-07-24 16:49:20 -0400

    addded sanity checker, updated model_test

--- sanityCheck.py
+++ sanityCheck.py
@@ -13,10 +13,17 @@ def main(argv):
     directory = argv[1]
 
     files = [x for x in os.listdir(directory) if ( x.endswith('.npy') and not x.startswith("IMG") )]
+
+    # check if each npy array is of the correct size
+
     random.shuffle(files)
-    files = files[:100]
 
     imgs = [np.load(os.path.join(directory, x), allow_pickle=True) for x in files]
+
+    for img in imgs:
+        assert img.shape == (512, 512, 3)
+    
+    imgs = imgs[:100]
 
     fig, axes = plt.subplots(10, 10, figsize=(10,10))
     for i, (im, ax, f) in enumerate(zip(imgs, axes.flatten(), files)):
