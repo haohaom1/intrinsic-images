fcefdd34bc69835fa34a2265e3322f1ec643e89e
Allen Ma <riallenma@gmail.com>
2019-07-24 13:11:22 -0400

    doc

--- makecolor_driver_test.py
+++ makecolor_driver_test.py
@@ -1,6 +1,20 @@
 import os
 
-os.system("python3 makecolor.py stripe 2000 test 512 512")
-os.system("python3 makecolor.py perlin 2000 test 512 512")
-os.system("python3 makecolor.py fractal 2000 test 512 512")
-os.system("python3 makecolor.py random 2000 test 512 512")
+
+
+import os
+
+def main(argv):
+    if len(argv) < 1:
+        print("usage: specify a basepath. It should be the parent folder of the data directory")
+    basepath = argv[1]
+    if not os.path.isdir(basepath):
+        print(f"{basepath} not a valid directory")
+        exit(-1)
+    os.system("python3 makecolor.py stripe 2000 test 512 512")
+    os.system("python3 makecolor.py perlin 2000 test 512 512")
+    os.system("python3 makecolor.py fractal 2000 test 512 512")
+    os.system("python3 makecolor.py random 2000 test 512 512")
+
+if __name__ == "__main__":
+    main(sys.argv)
